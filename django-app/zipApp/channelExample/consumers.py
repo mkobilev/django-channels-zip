c7ed9aa678ec2f8d8b2249d7e3e6a96dc1222288
Max Kobilev <mkobilev@gmail.com>
2018-10-31 19:30:17 +0300

    fix Pako decompressing

--- django-app/zipApp/channelExample/consumers.py
+++ django-app/zipApp/channelExample/consumers.py
@@ -4,10 +4,8 @@ import os
 import gzip
 
 with open(os.getcwd() + '/static/products100.json', 'r') as infile:
-
     json_file = json.load(infile)
-    json_file_gzip = gzip.compress( bytes(json.dumps(json_file), encoding='utf-8') )
-
+    json_file_gzip = gzip.compress(bytes(json.dumps(json_file), encoding='utf-8'))
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -19,7 +17,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self, close_code):
@@ -35,35 +32,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
 
         if 'getJson' in text_data_json['cmd']:
-            # Send message to room group
-            print('getJson')
+            # Send JSON
             await self.send(text_data=json.dumps(json_file))
 
-            # await self.channel_layer.group_send(self.room_group_name, { 'type': 'json_message', 'message': ''})
         elif 'getCompressedJson' in text_data_json['cmd']:
-            # Send message to room group
-            print('getCompressedJson')
-            print(len(json_file_gzip))
-
+            # Send compressed JSON
             await self.send(bytes_data=json_file_gzip)
-
-            # await self.channel_layer.group_send(self.room_group_name, { 'type': 'json_message', 'message': ''})
-
-
-
-    # Receive message from room group
-    async def json_message(self, event):
-        print('event', event)
-        json_event = event['json']
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps(json_file))
-
-
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
